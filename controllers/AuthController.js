529d28ab1fc5ef7948acd22939f2d84db6abdf0a
Arthur Danielson <arthurdanielson7@gmail.com>
2025-03-26 09:33:00 -0400

    finaliza autenticação

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -6,6 +6,39 @@ module.exports = class AuthController {
     res.render("auth/login");
   }
 
+  static async loginPost(req, res) {
+    const { email, password } = req.body;
+
+    // find user
+    const user = await User.findOne({ where: { email: email } });
+
+    if (!user) {
+      req.flash("message", "Usuário não encontrado!");
+      res.render("auth/register");
+
+      return;
+    }
+
+    // check if passwords match
+    const passwordMatch = bcrypt.compareSync(password, user.password);
+
+    if (!passwordMatch) {
+      req.flash("message", "Senha inválida!");
+      res.render("auth/login");
+
+      return;
+    }
+
+    // initialize session
+    req.session.userId = user.id;
+
+    req.flash("message", "Autenticação realizada com sucesso!");
+
+    req.session.save(() => {
+      res.redirect("/");
+    });
+  }
+
   static register(req, res) {
     res.render("auth/register");
   }
@@ -18,7 +51,47 @@ module.exports = class AuthController {
       req.flash("message", "As senhas não conferem, tente novamente!");
       res.render("auth/register");
 
-      return 
+      return;
     }
+
+    // check if user exists
+    const checkIfUserExists = await User.findOne({ where: { email: email } });
+
+    if (checkIfUserExists) {
+      req.flash("message", "O e-mail já está em uso!");
+      res.render("auth/register");
+
+      return;
+    }
+
+    // create a password
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const user = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    try {
+      const createdUser = await User.create(user);
+
+      // initialize session
+      req.session.userId = createdUser.id;
+
+      req.flash("message", "Cadastro realizado com sucesso!");
+
+      req.session.save(() => {
+        res.redirect("/");
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  static logout(req, res) {
+    req.session.destroy();
+    res.redirect("/login");
   }
 };
